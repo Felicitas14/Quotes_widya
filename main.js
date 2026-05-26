4884ce42abbcfd6281aa21682e80e3eacef51448
Widya <felicitaswidya2@gmail.com>
2026-05-26 21:39:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -747,7 +747,15 @@ ${crowns[index]}
 
 <div class="top-quote-actions">
 
-<span onclick="topLikeQuote('${quote.text}')">
+<span
+class="${
+likedQuotes.has(
+`${currentMood}_${quote.text.substring(0,15)}`
+)
+? 'liked-top'
+: ''
+}"
+onclick="topLikeQuote('${quote.text}')">span onclick="topLikeQuote('${quote.text}')">
 
 <i class="far fa-heart"></i>
 ${quote.likes || 0}
