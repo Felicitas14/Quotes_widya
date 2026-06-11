1c75973398193b0038fdf192520a5360f4e10398
Widya <felicitaswidya2@gmail.com>
2026-06-11 22:56:03 +0900

    Update main.js

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ const DEFAULT_QUOTES = {
         { text: "Jangan khawatir tentangku, khawatirlah tentang dirimu.", author: "Savage Queen", emoji: "🔥", likes: 650 },
         { text: "Aku tidak jahat, aku hanya tidak mudah dibohongi.", author: "Unknown", emoji: "🖤", likes: 538 },
         { text: "Kesuksesanku adalah balasan terbaik.", author: "Winner", emoji: "🏆", likes: 699 }
-        { text: "Hidup adalah permainan maka jadilah pemain bukan mainan.", author: "Drean resink", emoji: "😮‍💨", likes: 1409s }      
+        { text: "Hidup adalah permainan maka jadilah pemain bukan mainan.", author: "Drean resink", emoji: "😮‍💨", likes: 1409 }      
     ]
 };
 // State Variables
