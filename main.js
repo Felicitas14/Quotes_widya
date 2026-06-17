82b827018b9ec08ad1d0809c11a746c054bd31ed
Widya <felicitaswidya2@gmail.com>
2026-06-17 13:43:56 +0900

    Update main.js

--- main.js
+++ main.js
@@ -101,17 +101,12 @@ let savedQuotes = new Set(); // Store indices/keys of saved quotes
 let isAdmin = false;
 
 // Load from LocalStorage or initialize with defaults
-function initStorage() {
-    // Quotes DB
-    const savedDb = localStorage.getItem('quotes_db');
-    
-    if (savedDb) {
-        quotesDb = JSON.parse(savedDb);
-    } else {
-        quotesDb = JSON.parse(JSON.stringify(DEFAULT_QUOTES));
-        localStorage.setItem('quotes_db', JSON.stringify(quotesDb));
-    }
-}
+function initStorage() { // Quotes DB 
+    const savedDb = localStorage.getItem('quotes_db'); 
+    if (savedDb) { quotesDb = JSON.parse(savedDb); }
+                  
+        else { quotesDb = JSON.parse(JSON.stringify(DEFAULT_QUOTES)); // Deep copy 
+        localStorage.setItem('quotes_db', JSON.stringify(quotesDb)); }
 
     // Likes
     const savedLikes = localStorage.getItem('liked_quotes');
